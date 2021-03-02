b089ed0fead15ee8afdd535155dab9566ec1f8bd
jongin <bill1224@naver.com>
2021-03-02 19:53:53 +0900

    03/02 api Object -> toString

---
+++ api_object/SmartPhoneExample.java
@@ -0,0 +1,10 @@
+package api_object;
+
+public class SmartPhoneExample {
+	public static void main(String[] args) {
+		SmartPhone myPhone = new SmartPhone("구글", "안드로이드");
+		
+		String strObj = myPhone.toString();
+		System.out.println(strObj);
+	}
+}
